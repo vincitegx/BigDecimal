4847ff2ec0073b1972f9a921bdd3d6b27754e933
David Tega <40434274+vincitegx@users.noreply.github.com>
2022-05-26 02:37:34 +0100

    Add files via upload

---
+++ BigDecimal/src/bigdecimal/BigDecimal.java
@@ -0,0 +1,38 @@
+
+package bigdecimal;
+
+import java.math.BigInteger;
+import javafx.geometry.Point3D;
+
+public class BigDecimal {
+                                                                                               
+    public static void main(String[] args) {       
+        BigInteger a = new BigInteger("33327633730023");
+        BigInteger b = new BigInteger("3");
+        BigInteger c = a.multiply(b);
+        System.out.println(c);
+        BigDecimal d =  new BigDecimal();
+        String s = "ijfofi";
+        String[] tokens = "Java#HTML#Perl".split("#");
+        for (String token : tokens) {
+            System.out.println(token + " ");
+        }
+            String[] tokensv = "Java,C?C#,C++".split("[.,:;?]");
+        for (int i = 0; i < tokensv.length; i++)
+             System.out.println(tokensv[i]);
+        }
+      String s = "a+b$#c".replaceAll("[$+#]", "NNN");
+        char [] chars = "java1301".toCharArray();
+
+      char [] dst = {'j','a','v','a','1','3','0','1'};
+      String str =String.valueOf("5.33");
+      Point3D ab = new Point3D(1.0,1.2,1.5);
+      
+      
+      
+
+    
+        
+    
+    
+}
